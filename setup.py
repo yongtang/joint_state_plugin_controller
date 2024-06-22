4c312bd49981300bb467e16a1ea4f553ebdf9609
Yong Tang <yong.tang.github@outlook.com>
2024-06-22 03:04:19 +0000

    Add GitHub CI

--- setup.py
+++ setup.py
@@ -2,32 +2,31 @@ from setuptools import find_packages, setup
 
 import os, glob
 
-package_name = 'joint_state_plugin_controller'
+package_name = "joint_state_plugin_controller"
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
         (
             os.path.join("share", package_name, "launch"),
             glob.glob(os.path.join("launch", "launch.*.py")),
         ),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='Yong Tang',
-    maintainer_email='yong.tang.github@outlook.com',
-    description='TODO: Package description',
-    license='Apache-2.0',
-    tests_require=['pytest'],
+    maintainer="Yong Tang",
+    maintainer_email="yong.tang.github@outlook.com",
+    description="TODO: Package description",
+    license="Apache-2.0",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'controller = joint_state_plugin_controller.controller:main',
-            'mock = joint_state_plugin_controller.mock:main',
+        "console_scripts": [
+            "controller = joint_state_plugin_controller.controller:main",
+            "mock = joint_state_plugin_controller.mock:main",
         ],
     },
 )
